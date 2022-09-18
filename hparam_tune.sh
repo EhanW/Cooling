5663e972e86c2c126e576a2caaf6d00a4b955a12
yhwang <yihanwang@amss.ac.cn>
2022-09-18 16:08:37 +0800

    args

--- hparam_tune.sh
+++ hparam_tune.sh
@@ -1,8 +1,12 @@
 (
 python at_cooling.py --device cuda:1 --cooling-ratio 0 --cooling-interval 201 --cooling-start-epoch 201
-python at_cooling.py --device cuda:1 --cooling-ratio 0.1 --cooling-interval 10 --cooling-start-epoch 1
+python at_cooling.py --device cuda:1 --cooling-ratio 0.2 --cooling-interval 5 --cooling-start-epoch 50
 )&
 (
-python at_cooling.py --device cuda:2 --cooling-ratio 0.2 --cooling-interval 10 --cooling-start-epoch 1
-python at_cooling.py --device cuda:2 --cooling-ratio 0.3 --cooling-interval 10 --cooling-start-epoch 1
+python at_cooling.py --device cuda:2 --cooling-ratio 0.2 --cooling-interval 10 --cooling-start-epoch 50
+python at_cooling.py --device cuda:2 --cooling-ratio 0.2 --cooling-interval 10 --cooling-start-epoch 100
+)&
+(
+python at_cooling.py --device cuda:3 --cooling-ratio 0.4 --cooling-interval 10 --cooling-start-epoch 50
+python at_cooling.py --device cuda:3 --cooling-ratio 0.4 --cooling-interval 10 --cooling-start-epoch 100
 )&
